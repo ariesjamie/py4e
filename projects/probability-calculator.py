b2bebd92d89af339e9c51c097f96d917f4aa4b46
Jamie Liu <aries_jamie@outlook.com>
2022-05-02 14:53:57 -0400

    Update probability-calculator.py

--- projects/probability-calculator.py
+++ projects/probability-calculator.py
@@ -1,77 +1,50 @@
+
 import copy
 import random
+from collections import Counter
 
 class Hat:
-    def __init__(self, **kwargs):                           # hat1 = Hat(yellow=3, blue=2, green=6)
+    def __init__(self, **kwargs):
         self.contents = []
-        for key, value in kwargs.items():                   # for key, value in {"yellow":3, "blue":2, "green":6}
-            for _ in range(value):
+        for key, value in kwargs.items():
+            for _ in range(value):               
                 self.contents.append(key)
-        print(self.contents)
+        
 
     def draw(self, num_balls_drawn):
-        draw_balls_list = []
+        balls_drawn_list = []
         if num_balls_drawn > len(self.contents):
             return self.contents
         else:
             for _ in range(num_balls_drawn):
-                balls_index = random.randrange(len(self.contents))         ## pick one to remove it from the self.contents
-                ##print("index:",balls_index)
-                # balls_pick = self.contents[balls_index]
-                # print("pick:",balls_pick)
-                # self.contents.pop(balls_index)
-                # print(self.contents)
-                draw_balls_list.append(self.contents.pop(balls_index))                         ## add one to add it to the draw balls list
-                # print(self.contents)
-            return draw_balls_list
-        
+                draw_index = random.randrange(len(self.contents))
+                balls_drawn_list.append(self.contents.pop(draw_index)) 
+            return balls_drawn_list
+
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-
-    initial_contents = copy.deepcopy(hat.contents)  
-    expected_balls_count_M = 0
-
+    hat_copy = copy.deepcopy(hat.contents)
+    expected_balls_count = 0
     for _ in range(num_experiments):
-
-        # pick balls
-        hat.contents = copy.deepcopy(initial_contents)
-        # hat.contents = initial_contents
-        drawn_result = hat.draw(num_balls_drawn)
-
-        # convert list to dict
-        drawn_result_dict = convert_to_dict(drawn_result)
-        print(drawn_result_dict)
-
-        # compare two dicts
-        numbers_compare = len(expected_balls)
+        hat.contents = copy.deepcopy(hat_copy)
+        compare_num = len(expected_balls)
+        hat_drawn = hat.draw(num_balls_drawn)
+        # hat_experiment_dict = convert_to_dict(hat_drawn)
+        hat_drawn_dict = Counter(hat_drawn)
         for key, value in expected_balls.items():
-            # print(key, value)
-            if key in drawn_result_dict and value <= drawn_result_dict[key]:
-                numbers_compare -= 1
+            if key in hat_drawn_dict and value < hat_drawn_dict[key]:
+                compare_num -= 1    
 
-        if numbers_compare == 0:
-            expected_balls_count_M += 1
-
-    print(expected_balls_count_M)
-    return expected_balls_count_M/num_experiments
+        if compare_num == 0:
+            expected_balls_count += 1
+    return expected_balls_count / num_experiments
 
 
-
-    # return num_valid_experiments / num_experiments
-
-def convert_to_dict(list):
-    dict = {}
-    for item in list:
-        if item in dict:
-            dict[item] += 1
-        else:
-            dict[item] = 1
-
-    return dict 
-
-
-hat = Hat(yellow=3, blue=2, green=6, red = 5)
-# hat_draw = hat.draw(3)
-# print(hat_draw)
-
-experiment(hat, {"red":2,"green":2}, 5, 2000)
+# def convert_to_dict(list):
+#     dict = {}
+#     for item in list:
+#         if item in dict:
+#             dict[item] += 1
+#         else:
+#             dict[item] = 1
+#     return dict
